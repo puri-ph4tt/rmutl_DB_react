c227bcc65a03bb7c48cb3e8a5db923e8d35e3d9f
puri_ph4tt <puriphat001@gmail.com>
2021-12-06 10:53:34 +0700

    second try

--- server/app.js
+++ server/app.js
@@ -7,27 +7,25 @@ const mysql = require('mysql');
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
+
 var privateKey  = fs.readFileSync(__dirname + '/key.pem', 'utf8');
 var certificate = fs.readFileSync(__dirname + '/cert.pem', 'utf8');
+
 var credentials = {key: privateKey, cert: certificate};
 const app = express();
+
+var httpsServer = https.createServer(credentials, app);
 
 app.use(body());
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
-
-var httpsServer = https.createServer(credentials, app);
-
-//app.use(body());
-//app.use(express.static(path.resolve(__dirname, '..', 'build')));
-
 const db = mysql.createConnection({
     host: '172.29.64.1',
-    port: '3306',
     user: 'phatt',
     password: '1234',
     database: 'database_lab'
 });
+
 // show data
 app.get('/data', function(req,res){
     console.log("Hello in /data ");
@@ -82,9 +80,6 @@ app.post('/data', function(req, res){
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
-
-
-
 
 //module.exports = app;
 module.exports = httpsServer;
